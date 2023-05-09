e39add87ef7ee8c50f34d6726ae53140864304e5
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-09 13:24:19 -0500

    schema set for back end

--- models/music.js
+++ models/music.js
@@ -1,3 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const musicSchema = new Schema({
+  name: { type: String, require: true },
+  email: String,
+});
+
+const SpotifyModel = mongoose.model('Music', musicSchema);
+
+module.exports = SpotifyModel;
+
