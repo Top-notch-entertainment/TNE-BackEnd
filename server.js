4ee8d32947e2f28aa60b7ed4a8b8aed20af10302
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-09 11:16:44 -0500

    fill out server

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 'use strict';
 console.log('server running');
 
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.URL_DB
+)
+    .then(() => console.log("Mongo DB is conected!"))
+    .catch(e => console.log(e));
+
+
+
+const Music = require('./models/Music.js');
+const PORT = process.env.PORT || 5005;
+
+
+
+app.get('/', (request, response) => {
+    response.status(200).send('Welcome');
+});
+
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`));
