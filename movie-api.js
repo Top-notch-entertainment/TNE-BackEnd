b3e51665a43cd7414ff8cb0e5aca4d2e8b961fcf
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-11 14:18:22 -0500

    console log to see if we got data

--- movie-api.js
+++ movie-api.js
@@ -5,14 +5,14 @@ const axios = require('axios');
 //Movie route
 async function getMovies(request, response) {
     // console.log('TTTTTT', request.query);
-    let cityMovieSearch = request.query;
+    let cityMovieSearch = request.query.searchQuery;
+    console.log('city search', cityMovieSearch);
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${cityMovieSearch}&page=1&include_adult=false`;
     
     let movieSearch = await axios.get(url);
     // console.log('url return data:', movieSearch.data.results);
     
     let movieArray = movieSearch.data.results.map(movie => new Movie(movie));
-    console.log('data to pass front end', movieArray);
     response.status(200).send(movieArray);
 
 }
